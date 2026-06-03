5f222421e0d62af470a01bccc1ee0cf813e23cbe
yyc1029 <austinyin9201@gmail.com>
2026-06-03 16:55:43 +0800

    修正編輯單字部分會有文字凸出

--- WordCards/frmEditWord.Designer.cs
+++ WordCards/frmEditWord.Designer.cs
@@ -15,140 +15,157 @@ namespace WordCards
 
         private void InitializeComponent()
         {
-            this.grpWord      = new System.Windows.Forms.GroupBox();
-            this.txtWord      = new System.Windows.Forms.TextBox();
+            this.grpWord = new System.Windows.Forms.GroupBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
             this.grpPhonogram = new System.Windows.Forms.GroupBox();
             this.txtPhonogram = new System.Windows.Forms.TextBox();
             this.grpSoundPath = new System.Windows.Forms.GroupBox();
             this.txtSoundPath = new System.Windows.Forms.TextBox();
-            this.grpExplain   = new System.Windows.Forms.GroupBox();
-            this.txtExplain   = new System.Windows.Forms.TextBox();
-            this.btnSave      = new System.Windows.Forms.Button();
-            this.btnCancel    = new System.Windows.Forms.Button();
-
+            this.grpExplain = new System.Windows.Forms.GroupBox();
+            this.txtExplain = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.grpWord.SuspendLayout();
             this.grpPhonogram.SuspendLayout();
             this.grpSoundPath.SuspendLayout();
             this.grpExplain.SuspendLayout();
             this.SuspendLayout();
-
-            // ── 單字 ──
+            // 
+            // grpWord
+            // 
             this.grpWord.Controls.Add(this.txtWord);
             this.grpWord.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.grpWord.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
+            this.grpWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
             this.grpWord.Location = new System.Drawing.Point(30, 30);
             this.grpWord.Name = "grpWord";
-            this.grpWord.Size = new System.Drawing.Size(620, 100);
+            this.grpWord.Size = new System.Drawing.Size(620, 129);
             this.grpWord.TabIndex = 0;
             this.grpWord.TabStop = false;
             this.grpWord.Text = "單字（唯讀）";
-
-            this.txtWord.BackColor = System.Drawing.Color.FromArgb(236, 239, 241);
+            // 
+            // txtWord
+            // 
+            this.txtWord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(239)))), ((int)(((byte)(241)))));
             this.txtWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtWord.Enabled = false;
             this.txtWord.Font = new System.Drawing.Font("微軟正黑體", 13F, System.Drawing.FontStyle.Bold);
-            this.txtWord.ForeColor = System.Drawing.Color.FromArgb(21, 101, 192);
-            this.txtWord.Location = new System.Drawing.Point(20, 38);
+            this.txtWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(192)))));
+            this.txtWord.Location = new System.Drawing.Point(20, 46);
             this.txtWord.Name = "txtWord";
-            this.txtWord.Size = new System.Drawing.Size(578, 44);
+            this.txtWord.Size = new System.Drawing.Size(578, 54);
             this.txtWord.TabIndex = 0;
-
-            // ── 音標 ──
+            this.txtWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // grpPhonogram
+            // 
             this.grpPhonogram.Controls.Add(this.txtPhonogram);
             this.grpPhonogram.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.grpPhonogram.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
-            this.grpPhonogram.Location = new System.Drawing.Point(30, 148);
+            this.grpPhonogram.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.grpPhonogram.Location = new System.Drawing.Point(30, 165);
             this.grpPhonogram.Name = "grpPhonogram";
-            this.grpPhonogram.Size = new System.Drawing.Size(620, 100);
+            this.grpPhonogram.Size = new System.Drawing.Size(620, 114);
             this.grpPhonogram.TabIndex = 1;
             this.grpPhonogram.TabStop = false;
             this.grpPhonogram.Text = "音標";
-
+            // 
+            // txtPhonogram
+            // 
             this.txtPhonogram.BackColor = System.Drawing.Color.White;
             this.txtPhonogram.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtPhonogram.Font = new System.Drawing.Font("微軟正黑體", 13F);
-            this.txtPhonogram.ForeColor = System.Drawing.Color.FromArgb(0, 105, 92);
-            this.txtPhonogram.Location = new System.Drawing.Point(20, 38);
+            this.txtPhonogram.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(92)))));
+            this.txtPhonogram.Location = new System.Drawing.Point(20, 46);
             this.txtPhonogram.Name = "txtPhonogram";
-            this.txtPhonogram.Size = new System.Drawing.Size(578, 44);
+            this.txtPhonogram.Size = new System.Drawing.Size(578, 54);
             this.txtPhonogram.TabIndex = 0;
-
-            // ── 音檔路徑 ──
+            this.txtPhonogram.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // grpSoundPath
+            // 
             this.grpSoundPath.Controls.Add(this.txtSoundPath);
             this.grpSoundPath.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.grpSoundPath.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
-            this.grpSoundPath.Location = new System.Drawing.Point(30, 266);
+            this.grpSoundPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.grpSoundPath.Location = new System.Drawing.Point(30, 285);
             this.grpSoundPath.Name = "grpSoundPath";
-            this.grpSoundPath.Size = new System.Drawing.Size(620, 100);
+            this.grpSoundPath.Size = new System.Drawing.Size(620, 120);
             this.grpSoundPath.TabIndex = 2;
             this.grpSoundPath.TabStop = false;
             this.grpSoundPath.Text = "音檔路徑";
-
+            // 
+            // txtSoundPath
+            // 
             this.txtSoundPath.BackColor = System.Drawing.Color.White;
             this.txtSoundPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtSoundPath.Font = new System.Drawing.Font("微軟正黑體", 10F);
-            this.txtSoundPath.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
-            this.txtSoundPath.Location = new System.Drawing.Point(20, 38);
+            this.txtSoundPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.txtSoundPath.Location = new System.Drawing.Point(20, 57);
             this.txtSoundPath.Name = "txtSoundPath";
-            this.txtSoundPath.Size = new System.Drawing.Size(578, 42);
+            this.txtSoundPath.Size = new System.Drawing.Size(578, 43);
             this.txtSoundPath.TabIndex = 0;
-
-            // ── 解釋 ──
+            this.txtSoundPath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // grpExplain
+            // 
             this.grpExplain.Controls.Add(this.txtExplain);
             this.grpExplain.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.grpExplain.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
-            this.grpExplain.Location = new System.Drawing.Point(30, 384);
+            this.grpExplain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.grpExplain.Location = new System.Drawing.Point(30, 411);
             this.grpExplain.Name = "grpExplain";
-            this.grpExplain.Size = new System.Drawing.Size(620, 270);
+            this.grpExplain.Size = new System.Drawing.Size(620, 315);
             this.grpExplain.TabIndex = 3;
             this.grpExplain.TabStop = false;
             this.grpExplain.Text = "解釋";
-
+            // 
+            // txtExplain
+            // 
             this.txtExplain.BackColor = System.Drawing.Color.White;
             this.txtExplain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtExplain.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.txtExplain.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
+            this.txtExplain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
             this.txtExplain.Location = new System.Drawing.Point(20, 38);
             this.txtExplain.Multiline = true;
             this.txtExplain.Name = "txtExplain";
             this.txtExplain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtExplain.Size = new System.Drawing.Size(578, 210);
+            this.txtExplain.Size = new System.Drawing.Size(578, 257);
             this.txtExplain.TabIndex = 0;
-
-            // ── 儲存按鈕 ──
-            this.btnSave.BackColor = System.Drawing.Color.FromArgb(21, 101, 192);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(192)))));
             this.btnSave.DialogResult = System.Windows.Forms.DialogResult.Yes;
             this.btnSave.FlatAppearance.BorderSize = 0;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("微軟正黑體", 11F);
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(440, 675);
+            this.btnSave.Location = new System.Drawing.Point(440, 732);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(210, 52);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "💾  儲存";
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-
-            // ── 取消按鈕 ──
-            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(236, 239, 241);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(239)))), ((int)(((byte)(241)))));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.FlatAppearance.BorderSize = 0;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.Font = new System.Drawing.Font("微軟正黑體", 11F);
-            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(55, 71, 79);
-            this.btnCancel.Location = new System.Drawing.Point(30, 675);
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.btnCancel.Location = new System.Drawing.Point(30, 732);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(130, 52);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = false;
-
-            // ── 表單 ──
+            // 
+            // frmEditWord
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(682, 755);
+            this.ClientSize = new System.Drawing.Size(684, 796);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.grpExplain);
@@ -162,7 +179,6 @@ namespace WordCards
             this.Name = "frmEditWord";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "編輯單字";
-
             this.grpWord.ResumeLayout(false);
             this.grpWord.PerformLayout();
             this.grpPhonogram.ResumeLayout(false);
@@ -172,6 +188,7 @@ namespace WordCards
             this.grpExplain.ResumeLayout(false);
             this.grpExplain.PerformLayout();
             this.ResumeLayout(false);
+
         }
 
         #endregion
